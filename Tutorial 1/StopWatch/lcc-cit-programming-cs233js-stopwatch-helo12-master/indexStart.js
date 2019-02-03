14ee14d367c7a3c6c89f8abf26a2d38ddaac6ed0
Tim Allen <carver0428@hotmail.com>
2019-02-03 14:24:55 -0800

    Version 1 is done for lab 1 version 2 is 75% of the way done for both programs. Study hours were updated on the hours sheet.

--- Tutorial 1/StopWatch/lcc-cit-programming-cs233js-stopwatch-helo12-master/indexStart.js
+++ Tutorial 1/StopWatch/lcc-cit-programming-cs233js-stopwatch-helo12-master/indexStart.js
@@ -1,6 +1,28 @@
 // Create a class called StopWatch.
+class StopWatch
 {
+    constructor(start, stop, reset, minutes, seconds) {
+        this.start = start;
+        this.stop = stop;
+        this.reset = reset;
+        this.minutes = document.getElementById(minutes);
+        this.seconds = document.getElementById(seconds);
+
+        this.start = document.getElementById("start");
+        this.stop = document.getElementById("stop");
+        this.reset = document.getElementById("reset");
+
+        this.start.addEventListener("click",startTimer);
+        this.stop.addEventListener("click",stopTimer);
+        this.reset.addEventListener("click",resetTimer);
+       
+        this.start.onclick = this.startTimer.bind(this);
+        this.stop.onclick = this.stopTimer.bind(this);
+        this.reset.onclick = this.resetTimer.bind(this);
+    }
     /*
+      
+
         Add a constructor.  In the body of the constructor
         -   Create instance variables for these 3 variables as well
             as all of the elements on the page that you're going to
@@ -12,6 +34,81 @@
             event fires
             -- this.startButton.onclick = this.startTimer.bind(this);
     */
+    startTimer() {
+    if(this.isRunning == false){
+        this.isRunning=true;
+        this.interval = setInterval("incrementTimer()", 1000);
+       
+    }
+ 
+    this.timer= this.timerTime;
+    // if the timer is NOT running, start it
+    // call the function incrementTimer every second
+    // save the timer in a the timer variable
+console.log("start");
+}
+
+ incrementTimer() {
+    // increment the timerTime
+    // calculate the number of minutes and seconds
+    
+    this.timerTime++;
+
+
+    this.second.innerHTML = pad(timerTime);
+    this.minutes.innerHTML = pad(x);
+    if( this.timerTime > 60){
+        
+        this.second.innerHTML = 0;
+        this.timerTime=0;
+        this.x++;
+        this.minutes.innerHTML = x;
+      
+    }
+  
+}
+
+ pad(number) {
+    // add a leading 0 if the number is < 10
+   
+
+    if(timerTime<10){
+        this.second.innerHTML = "0" +timerTime;
+    }
+    else{
+        this.second.innerHTML = timerTime;
+    }
+    if(x<10){
+        return "0" +  this.x;
+    }
+    else{
+        return  this.x;
+    }
+  
+}
+
+ stopTimer() {
+    this.clearInterval(interval);
+    this.isRunning= false;
+    
+    // if the timer is running, stop it
+console.log("Stop");
+}
+
+
+resetTimer() {
+    // stop the timer
+    this.stopTimer();
+    this.timerTime = 0;
+    this.x=0;
+
+    this.second.innerHTML = "01";
+    this.minute.innerHTML = "00";
+    // set the timerTime back to 0
+    
+    // write 00 to the elements on the page for minutes and seconds
+console.log("reset");
+}
 
     /*
         Convert each function to a method.  
@@ -20,8 +117,10 @@
         -   Add this. in front of every variable and method
     */
 }
+var stopWatch;
+stopWatch = new StopWatch()
 
-// reate a variable called stopWatch
+// Create a variable called stopWatch
 
 // Add an event handler to the load event of the window. 
 // Use an anonymous function or an arrow function to
